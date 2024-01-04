9149a3327a0369770b77034537f45bceb36ba7ec
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-01-04 16:32:20 -0500

    add env variables

--- bbff.sh
+++ bbff.sh
@@ -3,14 +3,34 @@
 # Best Buffer Source Find BBSF
 #
 
-# Initial buffer size
-buffer_size=10000000
+# check for -h on the command line and output help if given and exit
+if [ "$1" == "-h" ]; then
+    echo "Usage: $0 [buffer_size] [increment_size] [packet_size] [packet_count]"
+    echo "  buffer_size:    The initial buffer size to start with"
+    echo "  increment_size: The amount to decrement the buffer size by"
+    echo "  packet_size:    The size of the packets to send"
+    echo "  packet_count:   The number of packets to send"
+    exit 0
+fi
+
+# check if environment overrides buffer size
+buffer_size=${buffer_size:-10000000}
+
+# check if increment size is defined
+increment_size=${increment_size:-125000}
+
+# check if environment overrides packet-size
+packet_size=${packet_size:-1500}
+
+# check if environment overrides packet-count
+packet_count=${packet_count:-1000000}
+
 # Loop until buffer size is greater than 0
 while [ $buffer_size -gt 0 ]
 do
     echo "$buffer_size bytes pcap buffer..."
     # Run the probe command with the current buffer size
-    output=$(sudo RUST_LOG=info target/release/probe --no-progress --packet-count 1000000 --packet-size 1500 --buffer-size $buffer_size)
+    output=$(sudo RUST_LOG=info target/release/probe --no-progress --packet-count $packet_count --packet-size $packet_size --buffer-size $buffer_size)
 
     # Extract packet loss information from the output
     received=$(echo "$output" | grep -oP 'Received: \K\d+')
@@ -21,5 +41,5 @@ do
     echo "Buffer Size: $buffer_size, Received: $received, Dropped: $dropped, Interface Dropped: $iface_dropped"
 
     # Decrement the buffer size
-    buffer_size=$((buffer_size - 125000))
+    buffer_size=$((buffer_size - increment_size))
 done
