07c3ba13013506545b1f1fa6bfa66929f461bbfb
Chris Kennedy <c@groovy.org>
2024-01-06 19:11:50 -0800

    use --smpte2110
    works now

--- capture_smpte2110.sh
+++ capture_smpte2110.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## SMPTE2110
-target/release/probe --no-progress --pcap-stats --batch-size 1 --buffer-size 10000000000 --pcap-channel-size 100000 --zmq-channel-size 10000 --packet-size 1250 --immediate-mode --no-zmq
+target/release/probe --no-progress --pcap-stats --smpte2110 --no-zmq
