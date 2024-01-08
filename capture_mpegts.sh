f85dd794612cf66cf24ad4b7cbef67fd0471c3ee
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-01-08 11:05:04 -0500

    show setup in more detail with helper scripts
    setup capture and monitoring options in init/runtime scripts.

--- capture_mpegts.sh
+++ capture_mpegts.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+ERROR_LEVEL=error
+CPU_BIND=0
+MEM_BIND=0
 
 ## MPEGTS
-target/release/probe --no-progress --pcap-stats --immediate-mode --send-raw-stream
+RUST_LOG=$ERROR_LEVEL numactl --cpubind=$CPU_BIND --membind=$MEM_BIND \
+    target/release/probe \
+        --no-progress \
+        --pcap-stats \
+        --immediate-mode \
+        --show-tr101290 \
+        --send-raw-stream \
+        --send-json-header
