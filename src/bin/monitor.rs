d08c101b94ab0db409485748b7d20fcb10aa458a
Chris Kennedy <c@groovy.org>
2024-03-31 01:44:31 -0700

    version 0.4.1

--- src/bin/monitor.rs
+++ src/bin/monitor.rs
@@ -434,7 +434,7 @@ async fn produce_message(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.4.0",
+    version = "0.4.1",
     about = "RsCap Monitor for ZeroMQ input of MPEG-TS and SMPTE 2110 streams from remote probe."
 )]
 struct Args {
