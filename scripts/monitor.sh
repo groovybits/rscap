576d59146837c33d001d2b105e296c08026e8df8
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-09 08:57:20 -0400

    fixup scripts for rpm build usage

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,6 +4,17 @@ BUILD=release-with-debug
 OUTPUT_FILE=images/test.jpg
 KAFKA_BROKER=sun:9092
 
-RUST_LOG=info target/$BUILD/monitor \
+GST_PLUGIN_PATH=/opt/rscap/lib64/gstreamer-1.0
+MONITOR_BIN=/target/$BUILD/monitor
+if [ -f "/opt/rscap/bin/probe" ]; then
+    MONITOR_BIN=/opt/rscap/bin/monitor
+fi
+
+GST_PLUGIN_PATH=$GST_PLUGIN_PATH \
+    GST_DEBUG=$GST_DEBUG_LEVEL \
+    RUST_BACKTRACE=$BACKTRACE \
+    RUST_LOG=$LOGLEVEL \
+    $MONITOR_BIN \
     --kafka-broker $KAFKA_BROKER \
+    --send-to-kafka \
     --output-file $OUTPUT_FILE $@
--- scripts/probe.sh
+++ scripts/probe.sh
@@ -8,8 +8,17 @@ SOURCE_IP=224.0.0.200
 SOURCE_DEVICE=eth0
 SOURCE_PORT=10000
 TARGET_PORT=5556
-sudo GST_DEBUG=$GST_DEBUG_LEVEL RUST_BACKTRACE=$BACKTRACE RUST_LOG=$LOGLEVEL \
-    target/$BUILD/probe \
+GST_PLUGIN_PATH=/opt/rscap/lib64/gstreamer-1.0
+PROBE_BIN=/target/$BUILD/probe
+if [ -f "/opt/rscap/bin/probe" ]; then
+    PROBE_BIN=/opt/rscap/bin/probe
+fi
+
+sudo GST_PLUGIN_PATH=$GST_PLUGIN_PATH \
+    GST_DEBUG=$GST_DEBUG_LEVEL \
+    RUST_BACKTRACE=$BACKTRACE \
+    RUST_LOG=$LOGLEVEL \
+    $PROBE_BIN \
     --pcap-stats \
     --source-ip $SOURCE_IP \
     --source-port $SOURCE_PORT \
