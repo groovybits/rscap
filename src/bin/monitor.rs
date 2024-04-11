da93d878da54da4f21a797e054f0529180fc81a7
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-11 06:19:34 -0400

    bump version 0.5.13
    fix log messages

--- src/bin/monitor.rs
+++ src/bin/monitor.rs
@@ -517,7 +517,7 @@ async fn produce_message(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.12",
+    version = "0.5.13",
     about = "RsCap Monitor for ZeroMQ input of MPEG-TS and SMPTE 2110 streams from remote probe."
 )]
 struct Args {
