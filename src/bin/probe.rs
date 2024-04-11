1fe81fee8e5b30255ff5d730d0a7f085a2f6c046
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-11 18:06:18 -0400

    version 0.5.20
    fix memory leak and busy loops

--- src/bin/probe.rs
+++ src/bin/probe.rs
@@ -467,7 +467,7 @@ fn init_pcap(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.19",
+    version = "0.5.20",
     about = "RsCap Probe for ZeroMQ output of MPEG-TS and SMPTE 2110 streams from pcap."
 )]
 struct Args {
