f71f4504c25df9fb20e02aa82fae7cda41924a81
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-11 08:23:18 -0400

    bump version to 0.5.15

--- src/bin/probe.rs
+++ src/bin/probe.rs
@@ -466,7 +466,7 @@ fn init_pcap(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.14",
+    version = "0.5.15",
     about = "RsCap Probe for ZeroMQ output of MPEG-TS and SMPTE 2110 streams from pcap."
 )]
 struct Args {
