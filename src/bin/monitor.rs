c9af63c4ce2464fb2ba3c7d78369e9a21abc43b1
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-12 09:03:54 -0400

    v0.5.29

--- src/bin/monitor.rs
+++ src/bin/monitor.rs
@@ -523,7 +523,7 @@ async fn produce_message(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.28",
+    version = "0.5.29",
     about = "RsCap Monitor for ZeroMQ input of MPEG-TS and SMPTE 2110 streams from remote probe."
 )]
 struct Args {
