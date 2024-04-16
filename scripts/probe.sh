66d0e0b2f1c31146421784f1a37fb3ccdc410a52
Chris Kennedy <c@groovy.org>
2024-04-15 19:25:33 -0700

    update probe.sh for using .env variables

--- scripts/probe.sh
+++ scripts/probe.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 #
-BUILD=release-with-debug
-GST_DEBUG_LEVEL=3
-BACKTRACE=full
-SOURCE_IP=224.0.0.200
-SOURCE_DEVICE=eth0
-SOURCE_PORT=10000
-TARGET_PORT=5556
+if [ "$RUST_BACKTRACE" = "" ]; then
+    BACKTRACE=full
+else
+    BACKTRACE=$RUST_BACKTRACE
+fi
+if [ -f ".env" ]; then
+    source .env
+fi
+if [ "$BUILD" == "" ]; then
+    BUILD=release-with-debug
+fi
+if [ "$GST_DEBUG_LEVEL" == "" ]; then
+    GST_DEBUG_LEVEL=3
+fi
+if [ "$SOURCE_IP" == "" ]; then
+    SOURCE_IP=224.0.0.200
+fi
+if [ "$SOURCE_DEVICE" == "" ]; then
+    SOURCE_DEVICE=eth0
+fi
+if [ "$SOURCE_PORT" == "" ]; then
+    SOURCE_PORT=10001
+fi
+if [ "$TARGET_PORT" == "" ]; then
+    TARGET_PORT=5556
+fi
+if [ "$TARGET_HOST" == "" ]; then
+    TARGET_HOST=0.0.0.0
+fi
 GST_PLUGIN_PATH=/opt/rscap/lib64/gstreamer-1.0
 LD_LIBRARY_PATH=/opt/rscap/lib64:$LD_LIBRARY_PATH
 PATH=/opt/rscap/bin:$PATH
@@ -23,11 +45,6 @@ echo "Using $PROBE_BIN"
 
 $PROBE_BIN -V
 
-#gst-inspect-1.0 ccdetect
-#gst-inspect-1.0 closedcaption
-#gst-inspect-1.0 ccextractor
-#gst-inspect-1.0 cc708overlay
-
 #VALGRIND="valgrind --show-leak-kinds=definite,possible --leak-check=full"
 
 sudo GST_PLUGIN_PATH=$GST_PLUGIN_PATH \
@@ -41,6 +58,7 @@ sudo GST_PLUGIN_PATH=$GST_PLUGIN_PATH \
     --source-device $SOURCE_DEVICE \
     --target-ip 0.0.0.0 \
     --send-null-packets \
+    --use-wireless \
     --target-port $TARGET_PORT \
     --extract-images \
     $@
