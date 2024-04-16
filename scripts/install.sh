7c3636d742a79672d7fe0d5ad457294030c51870
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-15 20:30:33 -0400

    bump version to 0.5.43

--- scripts/install.sh
+++ scripts/install.sh
@@ -431,11 +431,18 @@ touch gst-plugins-rs-installed.done
 
 # RsCap installation
 cd ..
+run_with_scl cargo clean
 run_with_scl cargo build --features gst --release
 
 # Copy RsCap binaries to the installation directory
-cp target/release/probe $PREFIX/bin/
-cp target/release/monitor $PREFIX/bin/
+cp -f target/release/probe $PREFIX/bin/
+cp -f target/release/monitor $PREFIX/bin/
+
+ls -altr $PREFIX/bin/probe
+ls -altr $PREFIX/bin/monitor
+
+probe -V
+monitor -V
 
 echo "------------------------------------------------------------"
 echo "GStreamer and essential dependencies installed."
