32a01759aac5143e1453f04099c09ac6be60acf3
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-17 13:16:13 -0400

    add pkg config path to setup env

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,3 +3,6 @@ GST_PLUGIN_PATH=$PREFIX/lib64/gstreamer-1.0
 LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH
 PATH=$PREFIX/bin:$PATH
 
+# For pkg-config to find .pc files
+export PKG_CONFIG_PATH=$PREFIX/lib64/pkgconfig:$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
