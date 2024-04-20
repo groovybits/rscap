663d29f33fe4f7c7f8b81e0e657afa8f371e67f4
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-20 15:03:09 -0400

    version 0.5.114

--- specs/rsprobe.spec
+++ specs/rsprobe.spec
@@ -1,5 +1,5 @@
 Name:           rsprobe
-Version:        0.5.113
+Version:        0.5.114
 Release:        1%{?dist}
 Summary:        MpegTS Stream Analysis Probe with Kafka and GStreamer
 License:        MIT
--- src/bin/probe.rs
+++ src/bin/probe.rs
@@ -465,7 +465,7 @@ fn init_pcap(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.113",
+    version = "0.5.114",
     about = "MpegTS Stream Analysis Probe with Kafka and GStreamer"
 )]
 struct Args {
