442aebc30c454f7e3b48f26453e6ef3a0ce1f0fd
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-23 23:51:30 -0400

    0.7.0

--- src/bin/probe.rs
+++ src/bin/probe.rs
@@ -494,7 +494,7 @@ async fn send_to_kafka(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.6.9",
+    version = "0.7.0",
     about = "MpegTS Stream Analysis Probe with Kafka and GStreamer"
 )]
 struct Args {
