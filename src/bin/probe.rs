5a81736cefba0918ff589b3f09851a602a4816bf
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-04-30 12:02:12 -0400

    0.7.1
    less kafka traffic, reduce to 1 per second avg.

--- src/bin/probe.rs
+++ src/bin/probe.rs
@@ -494,7 +494,7 @@ async fn send_to_kafka(
 #[derive(Parser, Debug)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.7.0",
+    version = "0.7.1",
     about = "MpegTS Stream Analysis Probe with Kafka and GStreamer"
 )]
 struct Args {
