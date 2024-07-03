cedf07d8daae9e41a6dc5e73d57caae8dca3c0e6
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-07-03 13:14:46 -0400

    override autoconf with autoconf27 by PATH force

--- scripts/install.sh
+++ scripts/install.sh
@@ -284,7 +284,13 @@ if [ "$OS" = "Darwin" -o "$distro_type" = "alma" ]; then
         GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone https://github.com/zapping-vbi/zvbi.git
         cd zvbi
         git checkout v$LIBZVBI_VERSION
-        export AUTOCONF=autoconf27
+        ## Autoconf 27 override HACK
+        mkdir bin
+        echo "#!/bin/sh" > bin/autoconf
+        echo "autoconf27" >> bin/autoconf
+        chmod 755 bin/autoconf
+        export PATH=`pwd`/bin:$PATH
+        ## End HACK
         run_with_scl ./autogen.sh
         run_with_scl ./configure --prefix=$PREFIX
         run_with_scl make -j $CPUS --silent
